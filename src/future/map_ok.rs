92e24fe60cf2f26ea29517d27c1ca69df9d49fd9
EFanZh <efanzh@gmail.com>
2022-09-04 18:06:28 +0800

    Test `map_ok` size

--- src/future/map_ok.rs
+++ src/future/map_ok.rs
@@ -63,18 +63,22 @@ where
 mod tests {
     use crate::future::future_ext::FutureExt;
     use futures_core::FusedFuture;
-    use futures_util::future;
+    use futures_util::{future, TryFutureExt};
+    use std::mem;
+
+    fn plus_3(value: u32) -> u32 {
+        value + 3
+    }
 
     #[tokio::test]
     async fn test_map_ok() {
-        assert_eq!(future::ok::<u32, u32>(2).slim_map_ok(|value| value + 3).await, Ok(5));
-
-        assert_eq!(future::err::<u32, u32>(2).slim_map_ok(|value| value + 3).await, Err(2));
+        assert_eq!(future::ok::<_, u32>(2).slim_map_ok(plus_3).await, Ok(5));
+        assert_eq!(future::err::<_, u32>(2).slim_map_ok(plus_3).await, Err(2));
     }
 
     #[tokio::test]
     async fn test_map_ok_clone() {
-        let future = future::ok::<u32, u32>(2).slim_map_ok(|value| value + 3);
+        let future = future::ok::<_, u32>(2).slim_map_ok(plus_3);
         let future_2 = future.clone();
 
         assert_eq!(future.await, Ok(5));
@@ -83,10 +87,24 @@ mod tests {
 
     #[tokio::test]
     async fn test_map_ok_fused_future() {
-        let mut future = future::ok::<u32, u32>(2).slim_map_ok(|value| value + 3);
+        let mut future = future::ok::<_, u32>(2).slim_map_ok(plus_3);
 
         assert!(!future.is_terminated());
         assert_eq!((&mut future).await, Ok(5));
         assert!(future.is_terminated());
+    }
+
+    #[tokio::test]
+    async fn test_map_ok_is_slim() {
+        let make_base_future = || crate::future::ok::<u32, u32>(2);
+        let base_future = make_base_future();
+        let future_1 = make_base_future().slim_map_ok(plus_3);
+        let future_2 = make_base_future().map_ok(plus_3);
+
+        assert_eq!(mem::size_of_val(&base_future), mem::size_of_val(&future_1));
+        assert!(mem::size_of_val(&future_1) < mem::size_of_val(&future_2));
+        assert_eq!(base_future.await, Ok(2));
+        assert_eq!(future_1.await, Ok(5));
+        assert_eq!(future_2.await, Ok(5));
     }
 }
