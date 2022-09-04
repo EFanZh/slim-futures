caadc1caf6dc2f067190ba0624a55d4bd2680127
EFanZh <efanzh@gmail.com>
2022-09-04 19:03:20 +0800

    Test `ready`

--- src/future/ready.rs
+++ src/future/ready.rs
@@ -33,3 +33,35 @@ where
 {
     support::assert_future::<_, T>(Ready::new(value))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::future;
+    use std::mem;
+
+    #[tokio::test]
+    async fn test_ready() {
+        assert_eq!(future::ready::<u32>(2).await, 2);
+    }
+
+    #[tokio::test]
+    async fn test_ready_clone() {
+        let future = future::ready::<u32>(2);
+        let future_2 = future.clone();
+
+        assert_eq!(future.await, 2);
+        assert_eq!(future_2.await, 2);
+    }
+
+    #[tokio::test]
+    async fn test_ready_is_slim() {
+        let value: u32 = 2;
+        let future_1 = future::ready(value);
+        let future_2 = futures_util::future::ready(value);
+
+        assert_eq!(mem::size_of_val(&value), mem::size_of_val(&future_1));
+        assert!(mem::size_of_val(&future_1) < mem::size_of_val(&future_2));
+        assert_eq!(future_1.await, 2);
+        assert_eq!(future_2.await, 2);
+    }
+}
