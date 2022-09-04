4b0b51eab477d9579a10d2943f7c819c92cbaf79
EFanZh <efanzh@gmail.com>
2022-09-04 18:26:44 +0800

    Test `map_ok_async`

--- src/future/map_ok_async.rs
+++ src/future/map_ok_async.rs
@@ -13,6 +13,18 @@ struct MapOkAsyncFn<F, E> {
     _phantom: PhantomData<fn() -> E>,
 }
 
+impl<F, E> Clone for MapOkAsyncFn<F, E>
+where
+    F: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+            _phantom: PhantomData,
+        }
+    }
+}
+
 impl<T, F, E> FnMut1<T> for MapOkAsyncFn<F, E>
 where
     F: FnMut1<T>,
@@ -56,6 +68,19 @@ where
     }
 }
 
+impl<Fut, F, T, E> Clone for MapOkAsync<Fut, F>
+where
+    Fut: Clone + Future<Output = Result<T, E>>,
+    F: Clone + FnMut1<T>,
+    F::Output: Clone + Future,
+{
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
 impl<Fut, F, T, E> Future for MapOkAsync<Fut, F>
 where
     Fut: Future<Output = Result<T, E>>,
@@ -77,5 +102,53 @@ where
 {
     fn is_terminated(&self) -> bool {
         self.inner.is_terminated()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::future::future_ext::FutureExt;
+    use futures_core::FusedFuture;
+    use futures_util::future;
+    use std::mem;
+    use std::num::NonZeroU32;
+
+    fn plus_3(value: u32) -> impl FusedFuture<Output = u32> + Clone {
+        future::ready(value + 3)
+    }
+
+    #[tokio::test]
+    async fn test_map_ok_async() {
+        assert_eq!(future::ok::<_, u32>(2).slim_map_ok_async(plus_3).await, Ok(5));
+        assert_eq!(future::err::<_, u32>(2).slim_map_ok_async(plus_3).await, Err(2));
+    }
+
+    #[tokio::test]
+    async fn test_map_ok_async_clone() {
+        let future = future::ok::<_, u32>(2).slim_map_ok_async(plus_3);
+        let future_2 = future.clone();
+
+        assert_eq!(future.await, Ok(5));
+        assert_eq!(future_2.await, Ok(5));
+    }
+
+    #[tokio::test]
+    async fn test_map_ok_async_fused_future() {
+        let mut future = future::ok::<_, u32>(2).slim_map_ok_async(plus_3);
+
+        assert!(!future.is_terminated());
+        assert_eq!((&mut future).await, Ok(5));
+        assert!(future.is_terminated());
+    }
+
+    #[tokio::test]
+    async fn test_map_ok_async_is_slim() {
+        let make_base_future = || crate::future::ok::<_, u32>(NonZeroU32::new(2).unwrap());
+        let base_future = make_base_future();
+        let future = make_base_future().slim_map_ok_async(|_| crate::future::ready(()));
+
+        assert_eq!(mem::size_of_val(&base_future), mem::size_of_val(&future));
+        assert_eq!(base_future.await.map(NonZeroU32::get), Ok(2));
+        assert_eq!(future.await, Ok(()));
     }
 }
