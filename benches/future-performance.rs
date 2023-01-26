84636a1b781525f06eb3c6bf3a840b9a5915ca71
EFanZh <efanzh@gmail.com>
2023-01-26 22:31:02 +0800

    Fix benchmark groups

--- benches/future-performance.rs
+++ benches/future-performance.rs
@@ -1,6 +1,7 @@
 use core::future::{self, Future};
 use criterion::async_executor::FuturesExecutor;
-use criterion::Criterion;
+use criterion::measurement::Measurement;
+use criterion::{BenchmarkGroup, Criterion};
 use futures_util::{FutureExt, TryFutureExt};
 use slim_futures::future::FutureExt as SlimFutureExt;
 use std::future::Ready;
@@ -9,13 +10,13 @@ use std::{convert, hint};
 // `and_then_async`.
 
 fn benchmark_and_then_async_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
     name: &str,
-    c: &mut Criterion,
     mut f: impl FnMut(Ready<Result<u32, ()>>, fn(u32) -> Ready<Result<u32, ()>>) -> Fut,
 ) where
     Fut: Future<Output = Result<u32, ()>>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             f(
                 future::ready(hint::black_box(Ok(2))),
@@ -25,34 +26,30 @@ fn benchmark_and_then_async_with<Fut>(
     });
 }
 
-fn benchmark_async_block_and_then_async(c: &mut Criterion) {
-    benchmark_and_then_async_with("and_then_async - async block", c, |fut, f| async move {
+fn benchmark_and_then_async(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - and_then_async");
+
+    benchmark_and_then_async_with(&mut benchmark_group, "async block", |fut, f| async move {
         f(fut.await?).await
     });
-}
 
-fn benchmark_futures_and_then_async(c: &mut Criterion) {
-    benchmark_and_then_async_with("and_then_async - futures", c, TryFutureExt::and_then);
-}
+    benchmark_and_then_async_with(&mut benchmark_group, "futures", TryFutureExt::and_then);
 
-fn benchmark_slim_futures_and_then_async(c: &mut Criterion) {
-    benchmark_and_then_async_with("and_then_async - slim futures", c, SlimFutureExt::slim_and_then_async);
-}
+    benchmark_and_then_async_with(&mut benchmark_group, "slim-futures", SlimFutureExt::slim_and_then_async);
 
-criterion::criterion_group!(
-    benchmark_and_then_async,
-    benchmark_async_block_and_then_async,
-    benchmark_futures_and_then_async,
-    benchmark_slim_futures_and_then_async,
-);
+    benchmark_group.finish()
+}
 
 // `map`.
 
-fn benchmark_map_with<Fut>(name: &str, c: &mut Criterion, mut f: impl FnMut(Ready<u32>, fn(u32) -> u32) -> Fut)
-where
+fn benchmark_map_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
+    name: &str,
+    mut f: impl FnMut(Ready<u32>, fn(u32) -> u32) -> Fut,
+) where
     Fut: Future<Output = u32>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             f(
                 future::ready(hint::black_box(2)),
@@ -62,35 +59,32 @@ where
     });
 }
 
-fn benchmark_async_block_map(c: &mut Criterion) {
-    benchmark_map_with("map - async block", c, |fut, f| async move { f(fut.await) });
-}
+fn benchmark_map(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - map");
 
-fn benchmark_futures_map(c: &mut Criterion) {
-    benchmark_map_with("map - futures", c, FutureExt::map);
-}
+    benchmark_map_with(
+        &mut benchmark_group,
+        "async block",
+        |fut, f| async move { f(fut.await) },
+    );
 
-fn benchmark_slim_futures_map(c: &mut Criterion) {
-    benchmark_map_with("map - slim futures", c, SlimFutureExt::slim_map);
-}
+    benchmark_map_with(&mut benchmark_group, "futures", FutureExt::map);
 
-criterion::criterion_group!(
-    benchmark_map,
-    benchmark_async_block_map,
-    benchmark_futures_map,
-    benchmark_slim_futures_map,
-);
+    benchmark_map_with(&mut benchmark_group, "slim-futures", SlimFutureExt::slim_map);
+
+    benchmark_group.finish()
+}
 
 // `map_async`.
 
 fn benchmark_map_async_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
     name: &str,
-    c: &mut Criterion,
     mut f: impl FnMut(Ready<u32>, fn(u32) -> Ready<u32>) -> Fut,
 ) where
     Fut: Future<Output = u32>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             f(
                 future::ready(hint::black_box(2)),
@@ -100,35 +94,30 @@ fn benchmark_map_async_with<Fut>(
     });
 }
 
-fn benchmark_async_block_map_async(c: &mut Criterion) {
-    benchmark_map_async_with("map_async - async block", c, |fut, f| async move { f(fut.await).await });
-}
+fn benchmark_map_async(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - map_async");
 
-fn benchmark_futures_map_async(c: &mut Criterion) {
-    benchmark_map_async_with("map_async - futures", c, FutureExt::then);
-}
+    benchmark_map_async_with(&mut benchmark_group, "async block", |fut, f| async move {
+        f(fut.await).await
+    });
 
-fn benchmark_slim_futures_map_async(c: &mut Criterion) {
-    benchmark_map_async_with("map_async - slim futures", c, SlimFutureExt::slim_map_async);
-}
+    benchmark_map_async_with(&mut benchmark_group, "futures", FutureExt::then);
 
-criterion::criterion_group!(
-    benchmark_map_async,
-    benchmark_async_block_map_async,
-    benchmark_futures_map_async,
-    benchmark_slim_futures_map_async,
-);
+    benchmark_map_async_with(&mut benchmark_group, "slim-futures", SlimFutureExt::slim_map_async);
+
+    benchmark_group.finish()
+}
 
 // `map_err`.
 
 fn benchmark_map_err_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
     name: &str,
-    c: &mut Criterion,
     mut f: impl FnMut(Ready<Result<(), u32>>, fn(u32) -> u32) -> Fut,
 ) where
     Fut: Future<Output = Result<(), u32>>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             f(
                 future::ready(hint::black_box(Err(2))),
@@ -138,35 +127,30 @@ fn benchmark_map_err_with<Fut>(
     });
 }
 
-fn benchmark_async_block_map_err(c: &mut Criterion) {
-    benchmark_map_err_with("map_err - async block", c, |fut, f| async move { fut.await.map_err(f) });
-}
+fn benchmark_map_err(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - map_err");
 
-fn benchmark_futures_map_err(c: &mut Criterion) {
-    benchmark_map_err_with("map_err - futures", c, TryFutureExt::map_err);
-}
+    benchmark_map_err_with(&mut benchmark_group, "async block", |fut, f| async move {
+        fut.await.map_err(f)
+    });
 
-fn benchmark_slim_futures_map_err(c: &mut Criterion) {
-    benchmark_map_err_with("map_err - slim futures", c, SlimFutureExt::slim_map_err);
-}
+    benchmark_map_err_with(&mut benchmark_group, "futures", TryFutureExt::map_err);
 
-criterion::criterion_group!(
-    benchmark_map_err,
-    benchmark_async_block_map_err,
-    benchmark_futures_map_err,
-    benchmark_slim_futures_map_err,
-);
+    benchmark_map_err_with(&mut benchmark_group, "slim-futures", SlimFutureExt::slim_map_err);
+
+    benchmark_group.finish()
+}
 
 // `map_ok`.
 
 fn benchmark_map_ok_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
     name: &str,
-    c: &mut Criterion,
     mut f: impl FnMut(Ready<Result<u32, ()>>, fn(u32) -> u32) -> Fut,
 ) where
     Fut: Future<Output = Result<u32, ()>>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             f(
                 future::ready(hint::black_box(Ok(2))),
@@ -176,32 +160,32 @@ fn benchmark_map_ok_with<Fut>(
     });
 }
 
-fn benchmark_async_block_map_ok(c: &mut Criterion) {
-    benchmark_map_ok_with("map_ok - async block", c, |fut, f| async move { fut.await.map(f) });
+fn benchmark_map_ok(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - map_ok");
+
+    benchmark_map_ok_with(
+        &mut benchmark_group,
+        "async block",
+        |fut, f| async move { fut.await.map(f) },
+    );
+
+    benchmark_map_ok_with(&mut benchmark_group, "futures", TryFutureExt::map_ok);
+
+    benchmark_map_ok_with(&mut benchmark_group, "slim-futures", SlimFutureExt::slim_map_ok);
+
+    benchmark_group.finish()
 }
 
-fn benchmark_futures_map_ok(c: &mut Criterion) {
-    benchmark_map_ok_with("map_ok - futures", c, TryFutureExt::map_ok);
-}
+// `map_chain`.
 
-fn benchmark_slim_futures_map_ok(c: &mut Criterion) {
-    benchmark_map_ok_with("map_ok - slim futures", c, SlimFutureExt::slim_map_ok);
-}
-
-criterion::criterion_group!(
-    benchmark_map_ok,
-    benchmark_async_block_map_ok,
-    benchmark_futures_map_ok,
-    benchmark_slim_futures_map_ok,
-);
-
-// `map_map_map`.
-
-fn benchmark_map_map_map_with<Fut>(name: &str, c: &mut Criterion, mut f: impl FnMut(Ready<u32>, fn(u32) -> u32) -> Fut)
-where
+fn benchmark_map_chain_with<Fut>(
+    benchmark_group: &mut BenchmarkGroup<impl Measurement>,
+    name: &str,
+    mut f: impl FnMut(Ready<u32>, fn(u32) -> u32) -> Fut,
+) where
     Fut: Future<Output = u32>,
 {
-    c.bench_function(name, |b| {
+    benchmark_group.bench_function(name, |b| {
         b.to_async(FuturesExecutor).iter(|| {
             let identity = hint::black_box::<fn(_) -> _>(convert::identity);
 
@@ -210,38 +194,34 @@ where
     });
 }
 
-fn benchmark_async_block_map_map_map(c: &mut Criterion) {
-    benchmark_map_map_map_with(
-        "map_map_map - async block",
-        c,
-        |fut, f| async move { f(f(f(fut.await))) },
-    );
-}
+fn benchmark_map_chain(c: &mut Criterion) {
+    let mut benchmark_group = c.benchmark_group("future - map chain");
 
-fn benchmark_futures_map_map_map(c: &mut Criterion) {
-    benchmark_map_map_map_with("map_map_map - futures", c, |fut, f| fut.map(f).map(f).map(f));
-}
-
-fn benchmark_slim_futures_map_map_map(c: &mut Criterion) {
-    benchmark_map_map_map_with("map_map_map - slim futures", c, |fut, f| {
-        fut.slim_map(f).slim_map(f).slim_map(f)
+    benchmark_map_chain_with(&mut benchmark_group, "async block", |fut, f| async move {
+        f(f(f(f(fut.await))))
     });
-}
 
-criterion::criterion_group!(
-    benchmark_map_map_map,
-    benchmark_async_block_map_map_map,
-    benchmark_futures_map_map_map,
-    benchmark_slim_futures_map_map_map,
-);
+    benchmark_map_chain_with(&mut benchmark_group, "futures", |fut, f| {
+        fut.map(f).map(f).map(f).map(f)
+    });
+
+    benchmark_map_chain_with(&mut benchmark_group, "slim-futures", |fut, f| {
+        fut.slim_map(f).slim_map(f).slim_map(f).slim_map(f)
+    });
+
+    benchmark_group.finish()
+}
 
 // Main.
 
-criterion::criterion_main!(
+criterion::criterion_group!(
+    benchmarks,
     benchmark_and_then_async,
     benchmark_map,
     benchmark_map_async,
     benchmark_map_err,
     benchmark_map_ok,
-    benchmark_map_map_map,
+    benchmark_map_chain,
 );
+
+criterion::criterion_main!(benchmarks);
